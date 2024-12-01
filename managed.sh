c9b63d3f433b23d700f9568056d0e7512e2d3259
Azumi <azumitan67@gmail.com>
2024-12-01 04:26:46 -0800

    Update managed.sh

--- managed.sh
+++ managed.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
-##extemely managed server
-
+## Extremely Managed Server Script
 sudo apt update
+sudo apt install -y python3 python3-pip curl
 
-package_name=$(apt search python3-netifaces | grep -o '^python3-netifaces\S*')
+install_package_if_available() {
+    package_name=$(apt search "$1" 2>/dev/null | grep -o "^$1\S*")
+    if [[ -n "$package_name" ]]; then
+        sudo apt install -y "$package_name"
+    else
+        echo "Package $1 not found in the repositories."
+    fi
+}
 
-if [[ -n "$package_name" ]]; then
-    sudo apt install "$package_name"
-else
-    echo "Package python3-netifaces not found in the repositories."
-fi
+install_package_if_available "python3-netifaces"
+install_package_if_available "python3-colorama"
+install_package_if_available "python3-requests"
+sudo apt install -y python3-venv
+python3 -m venv /tmp/my_env
+source /tmp/my_env/bin/activate
 
-package_name=$(apt search python3-colorama | grep -o '^python3-colorama\S*')
-
-if [[ -n "$package_name" ]]; then
-    sudo apt install "$package_name"
-else
-    echo "Package python3-colorama not found in the repositories."
-fi
+pip install --upgrade pip
+pip install netifaces colorama requests
+pip list
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Azumi67/Backhaul_script/refs/heads/main/backhaul.sh)"
+deactivate
